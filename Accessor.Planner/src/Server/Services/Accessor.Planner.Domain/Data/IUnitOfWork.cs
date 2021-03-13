1c02cb21cb631e8e74df0ce3e9adaf99c74bd7c7
FERNANDO NOVACKI FREITAS <fernando.freitas3@fatec.sp.gov.br>
2021-03-12 21:23:27 -0300

    Added Pattern Repository and Unit of work

---
+++ Accessor.Planner/src/Server/Services/Accessor.Planner.Domain/Data/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Accessor.Planner.Domain.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
